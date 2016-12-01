564f353db175d30f9d8d4039a0688ab97900956a
Hack Reactor Students <accounts@hackreactor.com>
2016-11-30 16:36:03 -0800

    fix errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,6 +86,7 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
   ]);
 
+
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
       // add your production server task here
